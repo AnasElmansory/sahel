43332681ba38e68f3ae42d641566c42fd0a6fb29
AnasElmansory <ansmg5555@gmail.com>
2020-08-17 15:41:25 +0200

    delete or move some files

--- lib/data/repository/helper.dart
+++
@@ -1,8 +0,0 @@
-final String host = "covid-19-data.p.rapidapi.com";
-final String url = "https://covid-19-data.p.rapidapi.com/help/countries";
-final String apiKey = "fb607f40a7mshe9031b75f78ce40p132a44jsne7e1549d39c2";
-final Map<String, String> header = {
-  'x-rapidapi-key': apiKey,
-  "x-rapidapi-host": host,
-  "content-type": "application/json",
-};
